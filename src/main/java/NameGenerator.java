63e855e713b3cdce9f3af757bde36f477032eefb
Nikhil Sharma <nikhil_sharma@live.co.uk>
2018-05-21 15:16:43 +0100

    Initial commit. Adds NameGenerator class

---
+++ src/main/java/NameGenerator.java
@@ -0,0 +1,15 @@
+import java.util.ArrayList;
+
+public class NameGenerator {
+    private ArrayList<String> names;
+
+    public NameGenerator() {
+        this.names = new ArrayList<String>();
+    }
+
+    public ArrayList<String> getNames() {
+        return names;
+    }
+
+
+}
